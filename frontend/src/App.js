ea653a4aec6961a7c2424cd40fa3702fa4a0c389
Drew Griffin <drew.griffin.dev@gmail.com>
2025-02-16 07:58:32 -0600

    initial frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import * as sessionActions from "./store/session";
+import HomePage from "./components/HomePage";
+
+function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return isLoaded && (
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+
+    return(
+        <div>
+            <p>Whaaaaaaaaaat's Upppppppp!</p>
+        </div>
+    )
+}
+
+export default HomePage;
