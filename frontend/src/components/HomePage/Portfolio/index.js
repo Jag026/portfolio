f406069d6eff4ee8f020aca1e85098fdd5faeb83
Drew Griffin <drew.griffin.dev@gmail.com>
2025-02-17 23:04:52 -0600

    Finish portfolio and projects components

--- frontend/src/components/HomePage/Portfolio/index.js
+++ frontend/src/components/HomePage/Portfolio/index.js
@@ -9,6 +9,7 @@ const Portfolio = () => {
                 <p className="font-bold text-6xl">Portfolio and Projects</p>
                 <div className="w-[400px] mt-4 flex flex-col justify-center items-center">
                     <p className="text-center">I love programming and I love building things! Please contact me if you would like to learn more or have an idea in mind.</p>
+                    <p>Or check out my <a href="https://github.com/jag026" target="_blank" rel="noopener noreferrer">Github</a></p>
                 </div>
             </div>
             <div className="px-24">
