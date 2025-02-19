ac62ad2222d6613772216ab969ed2023d64e29f2
Drew Griffin <drew.griffin.dev@gmail.com>
2025-02-18 21:10:33 -0600

    Post deployment, fix intro text width

--- frontend/src/components/HomePage/Intro/index.js
+++ frontend/src/components/HomePage/Intro/index.js
@@ -7,7 +7,7 @@ const Intro = () => {
             <div className="grid grid-col-2">
                 <div className="w-[70%] text-[#edf6fc] col-start-1 flex flex-col justify-center items-start font-source-sans pl-4">
                     <p className="font-bold text-6xl">Automation Test Engineer</p>
-                    <p className="text-xl mt-8 font-thin opacity-80">Designing automated tests, reporting and resolving critical bugs, full-stack development.</p>
+                    <p className="text-xl w-[600px] mt-8 font-thin opacity-80">Designing automated tests, full-stack development, QA and load testing. Reporting and resolving critical bugs.</p>
                     <p className="mt-6">About Me ></p>
                 </div>
                 <div className="col-start-2 flex justify-center items-center">
