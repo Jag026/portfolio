f40054b8f53101082aff6bf1c8847ac6db33b95f
Drew Griffin <drew.griffin.dev@gmail.com>
2025-02-23 21:25:43 -0600

    Update pdf

--- backend/utils/fetchAi.js
+++ backend/utils/fetchAi.js
@@ -109,7 +109,7 @@ function sendResume(name, email) {
     }
   ];
 
-    const pdfPath = path.join(__dirname, "Drew-Griffin-Automation-And-Performance-Test-Engineer.pdf"); // Path to the PDF file
+    const pdfPath = path.join(__dirname, "Drew Griffin, Automation Test Engineer, Fullstack Developer.pdf"); // Path to the PDF file
     const pdfData = fs.readFileSync(pdfPath); // Read the PDF file
     const pdfBase64 = pdfData.toString("base64"); // Convert to Base64
   client
